2b3e12e4674860bdbb6ef1754f455001024715ca
naphattharawat <naphattharawat@gmail.com>
2018-06-24 22:01:08 +0700

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,7 +12,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/version', (req, res, next) => {
-  res.send({ ok: true, version: 'V1.2.0', build: '20180617' });
+  res.send({ ok: true, version: 'V1.3.0', build: '20180624' });
 });
 
 // export default router;
