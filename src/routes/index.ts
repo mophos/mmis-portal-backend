2fe21cd65cbf11896575b6a549f4f34877e13bd6
naphattharawat <naphattharawat@gmail.com>
2019-09-30 22:29:32 +0700

    merge conflict

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,11 +12,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/version', (req, res, next) => {
-<<<<<<< HEAD
   res.send({ ok: true, version: 'V3.4.0', build: '20190930' });
-=======
-  res.send({ ok: true, version: 'V3.3.1', build: '20190528' });
->>>>>>> be1e63f88e12d90f7c2d0e6fdc3e0d452bb07c0e
 });
 
 // export default router;
